22f1464d996fa79c57fce8dcca6bf618077c97d4
areligarciax <areligmx@outlook.com>
2023-03-01 12:20:24 -0600

    Práctica 14

---
+++ P14_Listas6-Append.py
@@ -0,0 +1,16 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 14 - Método Append()
+
+fruta = ['Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina', 'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+
+del fruta[4]
+del fruta[-5]
+
+fruta.remove("Granada")
+fruta.remove("Pomelo")
+fruta.remove("Manzana")
+
+fruta.append("Fresa")
+fruta.append("Manzana")
+
+print("Lista de frutas:", fruta)
