40e5b414222fda0a6d65e48a293a84fedaf39aea
areligarciax <areligmx@outlook.com>
2023-03-01 12:18:30 -0600

    Práctica 3

---
+++ P3_Strings2.py
@@ -0,0 +1,11 @@
+# Areli Sarai García Medina | 20310380
+# Práctica 3 - Strings - ¿Cómo concatenar?
+
+palabra1 = "Nombre:"
+palabra2 = "Apellidos:"
+nombres = "Areli Sarai"
+apellidos = "García Medina"
+
+texto = palabra1 + " " + nombres + "\n" + palabra2 + " " + apellidos
+
+print(texto)
