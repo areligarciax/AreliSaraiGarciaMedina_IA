625bc734e44824519143330d86f3ad5e623049cd
areligarciax <areligmx@outlook.com>
2023-03-01 12:18:40 -0600

    Práctica 4

---
+++ P4_Upper-Lower-y-Title.py
@@ -0,0 +1,13 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 4 - Métodos Upper(), Lower() y Title()
+
+texto1 = "areli sarai garcía medina"
+texto2 = "hoLA mUnDo"
+texto3 = "inteligencia artificial"
+texto4 = "CentRO dE ensEÑanZA TéCNicA iNDusTRIaL"
+texto5 = "ceti"
+
+print(texto1.title())
+print(texto2.lower())
+print(texto3.upper())
+print(texto4.title() + " (" + texto5.upper() + ")")
