5f419c43265c01ba48381600c3eaa35f04a09e88
areligarciax <areligmx@outlook.com>
2023-03-01 12:19:37 -0600

    Práctica 8

---
+++ P8_Floats-y-Round.py
@@ -0,0 +1,11 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 8 - Floats y Método Round()
+
+num1 = 98246/348
+num2 = 89732/67
+
+resultado = num1 + num2
+
+print("Número 1:", num1)
+print("Número 2:", num2)
+print("Resultado:", round(resultado,4))
