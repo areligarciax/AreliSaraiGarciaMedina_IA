fc1cebffaf4e7ece9f9cbbcde62cb1dac809c68a
areligarciax <areligmx@outlook.com>
2023-03-01 12:16:43 -0600

    Práctica 1

---
+++ P1_Variables.py
@@ -0,0 +1,8 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 1 - Variables
+
+mensaje = "Hola, me llamo Areli y estudio Ingeniería Mecatrónica, mi número de registro es:"
+
+registro = 20310380
+
+print(mensaje, registro)
