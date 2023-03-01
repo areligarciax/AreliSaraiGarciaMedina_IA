81c305cdfb2cbb88e3ae34f0b763fa892c0c7c2e
areligarciax <areligmx@outlook.com>
2023-03-01 12:19:27 -0600

    Práctica 7

---
+++ P7_Exponentes.py
@@ -0,0 +1,11 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 7 - ¿Cómo calcular exponentes?
+
+nelevado = 13 ** 6
+nelevado2 = 5 ** 5
+nelevado3 = 10 ** 7
+
+print("\t--> Números exponenciales\n")
+print("Número 1:",nelevado)
+print("Número 2:",nelevado2)
+print("Número 3:",nelevado3)
