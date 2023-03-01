bfb6afcbbcc90c1f76798241c7507d2954458088
areligarciax <areligmx@outlook.com>
2023-03-01 12:18:58 -0600

    Práctica 5

---
+++ P5_SaltodeLinea-y-Tabulaciones.py
@@ -0,0 +1,7 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 5 - Saltos de Línea y Tabulaciones
+
+nombre = "Mauricio Alejandro Cabrera Arellano"
+grado = "6E"
+
+print("\t\t\t\t---> Curso de Inteligencia Artificial\n\n\tProfesor: " + nombre + "\n\tGrado: " + grado)
