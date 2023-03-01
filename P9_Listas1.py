bd7e322f7d3ef050eef08c635bfd5db3de093a27
areligarciax <areligmx@outlook.com>
2023-03-01 12:19:45 -0600

    Práctica 9

---
+++ P9_Listas1.py
@@ -0,0 +1,10 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 9 - Crear listas y utilizarlas
+
+ceti = ['Inteligencia Artificial', '6E', 'Areli García', '20310380', 'Mauricio Cabrera']
+
+print("Materia:", ceti[0])
+print("Grado:", ceti[1])
+print("Nombre:", ceti[2])
+print("Registro:", ceti[3])
+print("Profesor:", ceti[4])
