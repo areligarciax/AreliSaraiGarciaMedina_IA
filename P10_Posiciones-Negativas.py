4286619f71891be2658a8a9190dfeae012789cc6
areligarciax <areligmx@outlook.com>
2023-03-01 12:19:55 -0600

    Práctica 10

---
+++ P10_Posiciones-Negativas.py
@@ -0,0 +1,9 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 10 - Posiciones Negativas
+
+fruta = ['Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina' ,'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+
+print("La primera fruta es:", fruta[0])
+print("La segunda fruta es:", fruta[1])
+print("La penúltima fruta es:", fruta[-2])
+print("La última fruta es:", fruta[-1])
