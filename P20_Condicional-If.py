3a0692a7062e019e2a443b7ab50bdc6d9dc4106d
areligarciax <areligmx@outlook.com>
2023-03-01 12:21:08 -0600

    Práctica 20

---
+++ P20_Condicional-If.py
@@ -0,0 +1,14 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 20 - Ccondicional If y operadores de comparación
+
+num1 = 20310380
+num2 = 130602
+
+num3 = 43243
+num4 = 43243
+
+if num1!=num2:
+    print("Número 1:", num1, "y número 2:", num2 ,"no son iguales")
+
+if num3==num4:
+    print("\nLos numeros son iguales y contienen:", num3, "y", num4)
