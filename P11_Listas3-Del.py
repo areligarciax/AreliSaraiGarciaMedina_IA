9fb7a703bd14c4a49274532bc230dbd870332a63
areligarciax <areligmx@outlook.com>
2023-03-01 12:20:05 -0600

    Práctica 11

---
+++ P11_Listas3-Del.py
@@ -0,0 +1,11 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 11 - Método Del()
+
+fruta = ['Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina' ,'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+
+del fruta[0]
+del fruta[7]
+del fruta[-5]
+del fruta[-2]
+
+print("Lista de frutas:", fruta)
