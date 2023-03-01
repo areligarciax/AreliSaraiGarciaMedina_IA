df5037998952c6d496f36dd2af5533c9d01f59ec
areligarciax <areligmx@outlook.com>
2023-03-01 12:18:15 -0600

    Práctica 2

---
+++ P2_Strings1.py
@@ -0,0 +1,11 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 2 - Strings
+
+"Esta es la práctica 2"
+'Esto es uno de los ejemplos'
+
+mensaje = 'Este es el "ejemplo 1"\n'
+mensaje2 = "Este es el 'ejemplo' número '2'"
+
+print(mensaje)
+print(mensaje2)
