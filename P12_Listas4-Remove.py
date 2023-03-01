7ed3c3f5c4cff7bf4b060a8baa65f757c7e8b719
areligarciax <areligmx@outlook.com>
2023-03-01 12:20:12 -0600

    Práctica 12

---
+++ P12_Listas4-Remove.py
@@ -0,0 +1,15 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 12 - Método Remove()
+
+fruta = ['Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina' ,'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+
+del fruta[0]
+del fruta[7]
+del fruta[-5]
+del fruta[-2]
+
+fruta.remove("Fresa")
+fruta.remove("Granada")
+fruta.remove("Pomelo")
+
+print("Lista de frutas:", fruta)
