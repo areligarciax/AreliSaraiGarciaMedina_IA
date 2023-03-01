0109689e2f4cf0117168cf13b0cf5465cb4dab6f
areligarciax <areligmx@outlook.com>
2023-03-01 12:20:53 -0600

    Práctica 18

---
+++ P18_Tuplas-y-Listas.py
@@ -0,0 +1,12 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 18 - Crear y manejar tuplas - Diferencias con las listas
+
+elementos = ['negro', 'azul', 'marrón', 'gris', 'verde', 'naranja', 'rosa', 'púrpura', 'rojo', 'blanco', 'amarillo', 'Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina', 'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+ceti = ("Inteligencia Artificial", "Areli García","Número de registro:", 20000000, 310000, 380)
+
+print("La lista de elementos que tenemos es:", elementos)
+print("La tupla que tenemos es:", ceti)
+
+print("\nMateria:", ceti[0])
+print("Alumna:", ceti[1])
+print(ceti[2], + ceti[3] + ceti[4] + ceti[5])
