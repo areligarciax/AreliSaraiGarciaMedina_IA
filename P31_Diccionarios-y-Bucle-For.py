6cb3da812e425546ffe334e0ee1e567b23ddd361
areligarciax <areligmx@outlook.com>
2023-03-02 21:08:07 -0600

    Práctica 31

---
+++ P31_Diccionarios-y-Bucle-For.py
@@ -0,0 +1,50 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 31 - Como usar diccionarios con el bucle For
+
+alumno1 = {
+    "Nombre": "Sofía Veazey",
+    "Registro": "20310136",
+    "Edad": "21",
+    "Nacionalidad": "Canadiense"
+}
+
+alumno2 = {
+    "Nombre": "Areli García",
+    "Registro": "20310380",
+    "Edad": "20",
+    "Nacionalidad": "Mexicana"
+}
+
+alumno3 = {
+    "Nombre": "Josué Medina",
+    "Registro": "20310037",
+    "Edad": "25",
+    "Nacionalidad": "Mexicana"
+}
+
+alumno4 = {
+    "Nombre": "Cecille Bechelani",
+    "Registro": "20310279",
+    "Edad": "20",
+    "Nacionalidad": "Italiana"
+}
+
+for x, y in alumno1.items():
+    print(x, ": ", y)
+
+print("\n")
+
+for x, y in alumno2.items():
+    print(x, ": ", y)
+
+print("\n")
+
+for x, y in alumno3.items():
+    print(x, ": ", y)
+
+print("\n")
+
+for x, y in alumno4.items():
+    print(x, ": ", y)
+
+print("\n")
