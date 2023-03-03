57e10e5a5f850e3d03f2c447e9808eeb2b3b3d64
areligarciax <areligmx@outlook.com>
2023-03-02 20:16:29 -0600

    Práctica 26

---
+++ P26_Bucle-While.py
@@ -0,0 +1,13 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 26 - Bucle While
+
+print("- Bienvenido al programa -\n")
+print("Para salir teclea la letra 'x'\n")
+print("Introduce un número entero para contar hasta él desde 0")
+
+num = int(input())
+x = 0
+
+while x <= num:
+    print(x)
+    x += 1
