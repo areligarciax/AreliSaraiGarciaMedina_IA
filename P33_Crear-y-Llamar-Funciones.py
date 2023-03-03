6296db842a637395c25325b447288841d4cc7597
areligarciax <areligmx@outlook.com>
2023-03-03 16:54:00 -0600

    Práctica 33

---
+++ P33_Crear-y-Llamar-Funciones.py
@@ -0,0 +1,14 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 33 - Como crear y llamar funciones
+
+print("\t\t\t- Base de Datos CETI COLOMOS-")
+
+def CETI (nombre, registro, carrera, turno):
+    print("\nNombre: " + nombre + "\nRegistro: " + registro + "\nCarrera: " + carrera + "\nTurno: " + turno)
+
+CETI("Areli García", "20310380", "Ingeniería Mecatrónica", "Vespertino")
+CETI("Cecille Bechelani", "20302975", "Ingeniería Industrial", "Vespertino")
+CETI("Arlet Chávez", "20318763", "Ingeniería Industrial", "Vespertino")
+CETI("Josué Medina", "39878383", "Desarrollo de Software", "Matutino")
+CETI("Sabrina Castellanos", "29328390", "Ingeniería Mecatrónica", "Matutino")
+CETI("Angel Espinoza", "8772765", "Ingeniería Civil", "Vespertino")
