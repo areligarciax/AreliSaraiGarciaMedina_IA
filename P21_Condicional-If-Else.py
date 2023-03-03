60e52dcbe9b38c90b3673557915dea2884a29d73
areligarciax <areligmx@outlook.com>
2023-03-02 20:15:48 -0600

    Práctica 21

---
+++ P21_Condicional-If-Else.py
@@ -0,0 +1,9 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 21 - Condicional If Else
+
+edad = int(input("¿Cuántos años tienes?\n"))
+
+if edad >= 18:
+    print("\nPuedes acceder al contenido debido a que eres mayor a 18 años:)")
+else:
+    print("\nAún eres menor de edad, por lo tanto no puedes acceder al contenido:(")
