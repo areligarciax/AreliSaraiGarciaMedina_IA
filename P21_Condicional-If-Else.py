0e95b06958b2671db04b4e8e60b231aa8b45fefd
areligarciax <areligmx@outlook.com>
2023-03-04 14:23:59 -0600

    Práctica 21

--- P21_Condicional-If-Else.py
+++ P21_Condicional-If-Else.py
@@ -10,7 +10,7 @@ edad = int(input("¿Cuántos años tienes?\n"))
 if edad >= 18:
     print("\nPuedes acceder al contenido debido a que eres mayor a 18 años:)")
 
-    
+
 # En caso contrario de ser menor, entrará al else e imprimirá el resultado
 else:
     print("\nAún eres menor de edad, por lo tanto no puedes acceder al contenido:(")
