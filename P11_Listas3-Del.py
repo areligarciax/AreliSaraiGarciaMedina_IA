371655af3ca4866a063792ca93d63c53f3a60818
areligarciax <areligmx@outlook.com>
2023-03-04 14:00:39 -0600

    Práctica 11

--- P11_Listas3-Del.py
+++ P11_Listas3-Del.py
@@ -1,11 +1,18 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 11 - Método Del()
 
+# Declaramos la lista con sus objetos
+
 fruta = ['Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina' ,'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+
+
+# Eliminamos el objeto en la posición que queramos
 
 del fruta[0]
 del fruta[7]
 del fruta[-5]
 del fruta[-2]
+
+# Imprimimos la lista sin los objetos eliminados previamente
 
 print("Lista de frutas:", fruta)
