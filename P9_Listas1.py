6e0c4569ddf14e0d79daee4a78066b1ea2aafae3
areligarciax <areligmx@outlook.com>
2023-03-04 13:58:20 -0600

    Práctica 9

--- P9_Listas1.py
+++ P9_Listas1.py
@@ -1,7 +1,11 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 9 - Crear listas y utilizarlas
 
+# Declaramos la lista con sus objetos
+
 ceti = ['Inteligencia Artificial', '6E', 'Areli García', '20310380', 'Mauricio Cabrera']
+
+# Imprimimos la lista mandando a llamar un objeto a la vez
 
 print("Materia:", ceti[0])
 print("Grado:", ceti[1])
