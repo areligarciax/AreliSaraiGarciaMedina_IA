8fa17bcdfb3a9a162ddb627bdd213b361c2541d9
areligarciax <areligmx@outlook.com>
2023-03-04 14:07:23 -0600

    Práctica 15

--- P15_Listas7-Insert.py
+++ P15_Listas7-Insert.py
@@ -1,17 +1,29 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 15 - Método Insert()
 
+# Creamos una lista con sus objetos
+
 fruta = ['Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina', 'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+
+
+# Eliminamos los objetos por su posición
 
 del fruta[4]
 del fruta[-5]
+
+# Eliminamos los objetos por string
 
 fruta.remove("Granada")
 fruta.remove("Pomelo")
 fruta.remove("Manzana")
 
+# Agregamos los objetos en la posición que queramos
+
 fruta.insert(4,"Coco")
 fruta.insert(-3, "Guayaba")
 fruta.insert(7, "Kiwi")
+
+
+# Imprimimos el contenido de la lista
 
 print("Lista de frutas:", fruta)
