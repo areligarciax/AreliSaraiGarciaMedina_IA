26686054ba48ead0179914028f0d8b08ccb0c285
areligarciax <areligmx@outlook.com>
2023-03-04 14:35:31 -0600

    Práctica 25

--- P25_Tips-Condicionales.py
+++ P25_Tips-Condicionales.py
@@ -3,21 +3,30 @@
 
 print("\t\t- B i e n v e n i d o -\n")
 print("Introduce el valor de 'x'")
+# Se crea la variable 'x' con entrada tipo entero
 x = int(input())
 
 print("Introduce el valor de 'y'")
+# Se crea la variable 'y' con entrada tipo entero
 y = int(input())
 
 print("Introduce el valor de 'z'")
+# Se crea la variable 'y' con entrada tipo entero
 z = int(input())
 
+# Se compara si 'x' es mayor a 'y' y mayor a 'z'
 if x > y and x > z:
     print("El valor de 'x':", x, "es mayor que el valor de 'y':", y, "y de 'z':", z)
 
+# Se compara si 'y' es mayor a 'x' y mayor a 'z'
 if y > x and y > z:
     print("El valor de 'y':", y, "es mayor que el valor de 'x':", x, "y de 'z':", z)
 
-if z > x and z > x:
+# Se compara si 'z' es mayor a 'x' y mayor a 'y'
+if z > x and z > y:
     print("El valor de 'z':", z, "es mayor que el valor de 'x':", x, "y de 'y':", y)
+
+
+# En caso contrario de haber dos o más valores iguales, se imprime lo siguiente
 
 else: print("\nEl valor de 'x', 'y' o 'z' son iguales")
