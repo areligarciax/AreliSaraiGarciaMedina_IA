93394e6b537665866c0d8b1a4732a6b36b4bb44a
areligarciax <areligmx@outlook.com>
2023-03-04 14:06:06 -0600

    Práctica 14

--- P14_Listas6-Append.py
+++ P14_Listas6-Append.py
@@ -1,16 +1,29 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 14 - Método Append()
 
+# Declaramos la lista con sus objetos
+
 fruta = ['Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina', 'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+
+
+# Eliminamos el objeto por su posición
 
 del fruta[4]
 del fruta[-5]
+
+# Eliminamos el objeto con string
 
 fruta.remove("Granada")
 fruta.remove("Pomelo")
 fruta.remove("Manzana")
 
+
+# Agregamos los objetos al final de la lista
+
 fruta.append("Fresa")
 fruta.append("Manzana")
+
+
+# Imprimimos la lista
 
 print("Lista de frutas:", fruta)
