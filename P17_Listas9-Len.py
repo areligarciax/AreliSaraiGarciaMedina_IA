c867a777f8e9c8e8ec49bd4cedd4ad8634c5bd6e
areligarciax <areligmx@outlook.com>
2023-03-04 14:12:57 -0600

    Práctica 17

--- P17_Listas9-Len.py
+++ P17_Listas9-Len.py
@@ -1,9 +1,16 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 17 - Método Len()
 
+# Creamos una lista con sus elementos
+
 elementos = ['negro', 'azul', 'marrón', 'gris', 'verde', 'naranja', 'rosa', 'púrpura', 'rojo', 'blanco', 'amarillo', 'Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina', 'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
 
+# Contamos el número de elementos con la función len()
+
 print("Número de elementos en la lista:", len(elementos))
+
+
+# Imprimimos el primer y último elemento de la lista
 
 print("\nEl último elemento de la lista es:", elementos[29])
 print("El primer elemento de la lista es:", elementos[-30])
