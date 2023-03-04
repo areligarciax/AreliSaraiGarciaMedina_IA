45a58714d051df7e78eb3547ca6e99dc2008d684
areligarciax <areligmx@outlook.com>
2023-03-04 14:02:01 -0600

    Práctica 12

--- P12_Listas4-Remove.py
+++ P12_Listas4-Remove.py
@@ -1,15 +1,23 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 12 - Método Remove()
 
+# Declaramos la lista con sus objetos
+
 fruta = ['Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina' ,'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+
+# Eliminamos los objetos que queramos con su posición en la lista
 
 del fruta[0]
 del fruta[7]
 del fruta[-5]
 del fruta[-2]
 
+# Eliminamos los objetos ingresando el string
+
 fruta.remove("Fresa")
 fruta.remove("Granada")
 fruta.remove("Pomelo")
+
+# Imprimimos la lista resultante
 
 print("Lista de frutas:", fruta)
