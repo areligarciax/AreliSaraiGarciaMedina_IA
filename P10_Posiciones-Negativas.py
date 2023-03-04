f79b68020801f00bbd8098e0e9e88f6d76f46cf7
areligarciax <areligmx@outlook.com>
2023-03-04 13:59:31 -0600

    Práctica 10

--- P10_Posiciones-Negativas.py
+++ P10_Posiciones-Negativas.py
@@ -1,9 +1,16 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 10 - Posiciones Negativas
 
+# Declaramos la lista con sus objetos
+
 fruta = ['Arándano', 'Cereza', 'Ciruela', 'Frambuesa', 'Fresa', 'Granada', 'Lima', 'Limón', 'Mandarina' ,'Manzana', 'Melón', 'Mora', 'Naranja', 'Pera', 'Piña', 'Plátano', 'Pomelo', 'Sandía', 'Uva']
+
+# Mandamos traer las primeras posiciones
 
 print("La primera fruta es:", fruta[0])
 print("La segunda fruta es:", fruta[1])
+
+# Mandamos llamar las últimas posiciones con los números negativos
+
 print("La penúltima fruta es:", fruta[-2])
 print("La última fruta es:", fruta[-1])
