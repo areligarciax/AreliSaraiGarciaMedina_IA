c8245a63162f0fd7305ce42ed2d4e41bcadd24ed
areligarciax <areligmx@outlook.com>
2023-03-04 14:38:38 -0600

    Práctica 26

--- P26_Bucle-While.py
+++ P26_Bucle-While.py
@@ -1,13 +1,21 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 26 - Bucle While
 
+# Se imrpimen los siguientes mensajes
 print("- Bienvenido al programa -\n")
 print("Para salir teclea la letra 'x'\n")
 print("Introduce un número entero para contar hasta él desde 0")
 
+
+# Se crea la variable 'num' con entrada tipo entero
 num = int(input())
+# Se inicializa 'x' con valor de 0
 x = 0
+
+# Se crea un bucle al que entrará hasta que 'x' llegue al número ingresado
+# Por lo tanto el programa imprimirá todos los valores hasta llegar al valor ingresado
 
 while x <= num:
     print(x)
+    # El valor de 'x' va sumando 1 cada vez que entra al bucle
     x += 1
