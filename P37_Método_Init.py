cfa13a8a576a7322b3ac07281a662c8bf7b57776
areligarciax <areligmx@outlook.com>
2023-03-03 18:01:24 -0600

    Práctica 37

---
+++ P37_Método_Init.py
@@ -0,0 +1,24 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 37 - El método __init__ - Programación orientada a objetos
+
+class Usuario():
+	def __init__(self, nombre, apellido, registro, turno):
+		self.nombre = nombre
+		self.apellido = apellido
+		self.registro = registro
+		self.turno = turno
+
+	def imprimir_datos(self):
+		print('\nNombre:', self.nombre, '\nApellido:', self.apellido, '\nRegistro:', self.registro,  '\nTurno:', self.turno)
+
+usuario1 = Usuario("Areli", "García", "20310380", "Vespertino")
+usuario1.imprimir_datos()
+
+usuario2 = Usuario("Cecille", "Bechelani", "2436689", "Matutino")
+usuario2.imprimir_datos()
+
+usuario3 = Usuario("Josué", "Medina", "256677", "Matutino")
+usuario3.imprimir_datos()
+
+usuario4 = Usuario("Sarah", "Veazey", "234567", "Vespertino")
+usuario4.imprimir_datos()
