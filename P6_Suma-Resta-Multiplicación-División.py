4dcccc05e29d905656372350bc1abe15ba9e3e07
areligarciax <areligmx@outlook.com>
2023-03-04 13:54:55 -0600

    Práctica 6

--- P6_Suma-Resta-Multiplicación-División.py
+++ P6_Suma-Resta-Multiplicación-División.py
@@ -1,10 +1,15 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 6 - Operadores (suma, resta, multiplicación y división)
 
-suma = 13+6+23
-resta = 432-43-67
-multiplicacion = 54*65*32
-division = 2548/54
+# Declaramos las variables
+
+suma = 13 + 6 + 23
+resta = 432 - 43 - 67
+multiplicacion = 54 * 65 * 32
+division = 2548 / 54
+
+
+# Imprimimos las variables con su resultado
 
 print("Suma:", suma)
 print("Resta:", resta)
