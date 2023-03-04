8029a815fda649e7f62169ec6370e3c100549811
areligarciax <areligmx@outlook.com>
2023-03-04 13:52:39 -0600

    Práctica 4

--- P4_Upper-Lower-y-Title.py
+++ P4_Upper-Lower-y-Title.py
@@ -1,13 +1,26 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 4 - Métodos Upper(), Lower() y Title()
 
+# Declaramos las variables
+
 texto1 = "areli sarai garcía medina"
 texto2 = "hoLA mUnDo"
 texto3 = "inteligencia artificial"
 texto4 = "CentRO dE ensEÑanZA TéCNicA iNDusTRIaL"
 texto5 = "ceti"
 
+# Primera letra de cada palabra en mayúscula
+
 print(texto1.title())
+
+# Texto en minúscula
+
 print(texto2.lower())
+
+# Texto en mayúscula
+
 print(texto3.upper())
+
+# Primera parte, primer letra en mayúscula de cada palabra; segunda parte todo en mayúscula
+
 print(texto4.title() + " (" + texto5.upper() + ")")
