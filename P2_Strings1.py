f9c73c1eb5c2c54e858c700edaf3d4c9fe695aaa
areligarciax <areligmx@outlook.com>
2023-03-04 13:47:58 -0600

    Práctica 2

--- P2_Strings1.py
+++ P2_Strings1.py
@@ -4,8 +4,15 @@
 "Esta es la práctica 2"
 'Esto es uno de los ejemplos'
 
+#Declaramos una variable con comillas simples ('')
+
 mensaje = 'Este es el "ejemplo 1"\n'
+
+#Declaramos una variable con comillas dobles ("")
+
 mensaje2 = "Este es el 'ejemplo' número '2'"
+
+# Imprimimos el resultado
 
 print(mensaje)
 print(mensaje2)
