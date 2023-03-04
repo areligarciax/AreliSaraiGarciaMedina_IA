41998c9d274c9f24a7553b73dba765823333e8a9
areligarciax <areligmx@outlook.com>
2023-03-04 14:46:18 -0600

    Práctica 28

--- P28_Bucle-For.py
+++ P28_Bucle-For.py
@@ -2,6 +2,10 @@
 # Práctica 28 - Bucle For
 
 print("\t\t- B I E N V E N I D O -\n")
+
+# Se crean las variables 'nombre', 'materia' y 'plantel' con entrada tipo string
+# Se crean las variable 'registro' y 'calificacion' con entrada tipo entero
+
 print("Ingresa tu nombre")
 nombre = input()
 
@@ -17,12 +21,17 @@ plantel = input()
 print("\nIngresa la calificación que obtuviste")
 calificacion = int(input())
 
+# Se crea una arreglo con los elementos solicitados en la parte superior
+
 datos = [nombre, registro, materia, plantel, calificacion]
 
 print("\n\t--> Por motivos de seguridad el registro no será mostrado")
 
+# Al mostrar los datos, la variable de 'registro' será ignorada y no será mostrada con ayuda del ciclo for
+
 for x in datos:
     if x == registro:
         continue
+    # Por cada dato ingresado, se mostrará el siguiente mensaje
     print("\nLos datos ingresados son:")
     print(x)
