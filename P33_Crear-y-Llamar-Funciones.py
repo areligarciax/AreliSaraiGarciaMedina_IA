b7ad5f4bee3cc035a4d6584894e8c39a0be49873
areligarciax <areligmx@outlook.com>
2023-03-05 12:28:35 -0600

    Práctica 33

--- P33_Crear-y-Llamar-Funciones.py
+++ P33_Crear-y-Llamar-Funciones.py
@@ -1,10 +1,18 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 33 - Como crear y llamar funciones
 
+
+# Se imprime el siguiente mensaje
+
 print("\t\t\t- Base de Datos CETI COLOMOS-")
 
+
+# Se crea la función ceti pasandole los siguietes parámetros: 'nombre', 'registro', 'carrera' y 'turno'
 def CETI (nombre, registro, carrera, turno):
     print("\nNombre: " + nombre + "\nRegistro: " + registro + "\nCarrera: " + carrera + "\nTurno: " + turno)
+
+
+# Se manda a llamar la función CETI() y se declara lo que habrá dentro de ellos
 
 CETI("Areli García", "20310380", "Ingeniería Mecatrónica", "Vespertino")
 CETI("Cecille Bechelani", "20302975", "Ingeniería Industrial", "Vespertino")
