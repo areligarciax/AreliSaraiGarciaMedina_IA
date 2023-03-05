ec9ded0c66fa1b553780b8a3c8e133d476c8067e
areligarciax <areligmx@outlook.com>
2023-03-05 13:46:30 -0600

    Práctica 48

--- P48_Split-y-Sub.py
+++ P48_Split-y-Sub.py
@@ -1,20 +1,26 @@
 # Areli Sarai García Medina | 203010380
 # Práctica 48 - Expresiones regulares - split() y sub() - RegEx
 
+# Se importa la librería 're' para las operaciones de coincidencia de expresiones regulares
 import re
 
+# Se declara el 'texto1' como string
 texto1 = "Areli Sarai García Medina - 20310380 - Ingeniería Mecatrónica"
 
+# Se separará cada espacio ' ' del texto
 resultado = re.split(" ", texto1)
 print(resultado)
 
+# Se separará cada espacio ' ' del texto, limitándolo a las primeras 5 coincidencias
 resultado2 = re.split(" ", texto1, 5)
 print(resultado2)
 
 print("\n\n")
 
+# Se sustituirá cada espacio ' ' por '/' en el texto
 resultado3 = re.sub(" ", "/", texto1)
 print(resultado3)
 
-resultado4 = re.sub(" ", "/", texto1, 5)
+# Se sustituirá cada espacio ' ' por '/' en el texto, limitándolo a las primeras 6 coincidencias
+resultado4 = re.sub(" ", "/", texto1, 6)
 print(resultado4)
