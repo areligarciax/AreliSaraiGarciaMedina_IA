4d5a833c5a5b5b85b2f529b0ed447b8317cdb1fe
areligarciax <areligmx@outlook.com>
2023-03-15 13:10:06 -0600

    Práctica 5

---
+++ P05_SaltodeLinea-y-Tabulaciones.py
@@ -0,0 +1,9 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 5 - Saltos de Línea y Tabulaciones
+
+nombre = "Mauricio Alejandro Cabrera Arellano"
+grado = "6E"
+
+# Imprimimos el texto con tabulaciones (\t) y con saltos de linea (\n)
+
+print("\t\t\t\t---> Curso de Inteligencia Artificial\n\n\tProfesor: " + nombre + "\n\tGrado: " + grado)
