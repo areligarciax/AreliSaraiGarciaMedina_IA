3d2ad327eac7b15d34c2fbb6b1911c31d9944613
areligarciax <areligmx@outlook.com>
2023-03-15 13:09:34 -0600

    Práctica 1

---
+++ P01_Variables.py
@@ -0,0 +1,10 @@
+# Areli Sarai García Medina | 203010380
+# Práctica 1 - Variables
+
+# Declaramos las variables
+mensaje = "Hola, me llamo Areli y estudio Ingeniería Mecatrónica, mi número de registro es:"
+
+registro = 20310380
+
+# Aquí imprimimos la variable string y la variable int
+print(mensaje, registro)
